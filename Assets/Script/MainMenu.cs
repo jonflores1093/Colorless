a3c54eec499ce48d400c60b913fdd177f3e1f28a
Jon Flores <61673020+jonflores1093@users.noreply.github.com>
2020-12-03 20:14:27 -0600

    Tried to use datetime in script

--- Assets/Script/MainMenu.cs
+++ Assets/Script/MainMenu.cs
@@ -2,14 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System;
 
 public class MainMenu : MonoBehaviour
 {
-    
+
+    private DateTime sceneTime = 18:30:00f;
+
     public void PlayGame()
     {
+        TimeZoneInfo cst = TimeZoneInfo.FindSystemTimeZoneById("Central Standard Time");
+        System.DateTime today = System.DateTime.Now;
+        DateTime currentTime = TimeZoneInfo.ConvertTime(today, cst);
+        Debug.Log(currentTime);
 
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        if (currentTime == 18:12:00)
+        {
+
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+
+        }
 
 
 
@@ -28,4 +41,12 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene("Menu");
 
     }
+
+
+
+    
+
+
+
+
 }
